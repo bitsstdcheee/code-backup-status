1b4531dfd5ff3fcd992051b3663ff7dde77d395d
Dell-G3 <Jestingaero@163.com>
2023-06-21 22:57:46 +0800

    去除 br

--- luogu/punch.py
+++ luogu/punch.py
@@ -22,7 +22,7 @@ def punch(cookie):
         "Sec-Ch-Ua-Platform": "Windows",
         "Accept": "*/*",
         "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
-        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Encoding": "gzip, deflate",
         "Connection": "keep-alive",
         "Referer": "https://www.luogu.com.cn/",
         "Cache-Control": "no-cache",
